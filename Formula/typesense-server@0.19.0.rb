a2f68122063bb1cd25760875a5b1bdba95301572
Jason Bosco <mail@jasonbos.co>
2022-05-31 19:29:13 -0500

    Add service

--- Formula/typesense-server@0.19.0.rb
+++ Formula/typesense-server@0.19.0.rb
@@ -8,6 +8,42 @@ class TypesenseServerAT0190 < Formula
 
   def install
     bin.install "typesense-server"
+
+    # Create directories
+    [data_dir, config_dir, log_dir].each(&:mkpath)
+
+    # Create default config file
+    config_file_contents = <<~EOF
+      [server]
+      api-address = 0.0.0.0
+      api-port = 8108
+      api-key = xyz
+      data-dir = #{var}/lib/typesense
+      log-dir = #{var}/log/typesense
+      enable-cors = true
+    EOF
+    system "echo '#{config_file_contents}' > #{buildpath/"typesense.ini"}"
+    config_dir.install "typesense.ini"
+  end
+
+  service do
+    run [bin/"typesense-server", "--config=#{etc/"typesense/typesense.ini"}"]
+    keep_alive true
+    error_log_path var/"log/typesense.log"
+    log_path var/"log/typesense.log"
+    working_dir var/"lib/typesense"
+  end
+
+  def config_dir
+    etc/"typesense"
+  end
+
+  def data_dir
+    var/"lib/typesense"
+  end
+
+  def log_dir
+    var/"log/typesense"
   end
 
   test do
